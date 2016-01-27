2333c0e154cfaaac87f5b32ba28f63d03e96b0c1
thyrst <thyrst@seznam.cz>
2016-01-28 00:50:13 +0100

    new test

---
+++ tests/test_end2end.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+DHTMLParserPy example how to find every link in document.
+"""
+
+import urllib.request
+import dhtmlparser
+
+def test_get_links():
+    f = urllib.request.urlopen("http://google.com")
+    data = f.read()
+    f.close()
+
+    dom = dhtmlparser.parseString(data)
+
+    for link in dom.find("a"):
+    	if "href" in link.params:
+    		assert isinstance(link.params["href"], list)
